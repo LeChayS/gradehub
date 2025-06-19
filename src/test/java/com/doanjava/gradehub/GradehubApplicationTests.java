d01608da9cf8e28d50bbe13772a72f4871be2efb
Lê Thị Tuyết Trâm <142717464+LeChayS@users.noreply.github.com>
2025-06-19 09:28:17 +0700

    first commit

---
+++ src/test/java/com/doanjava/gradehub/GradehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doanjava.gradehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
